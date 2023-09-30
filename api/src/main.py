373406316c984fb1c373652a8aa39c16b3eb1bb9
piotr <piotr.las@protonmail.com>
2023-09-30 21:48:17 +0200

    add cors to main

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import create_all
 from .projects import router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router)
 
