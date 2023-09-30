a2ab51fe2a77901a96412b8e339106680ac6993c
piotr <piotr.las@protonmail.com>
2023-10-01 01:07:21 +0200

    add courses to orms

--- api/src/models.py
+++ api/src/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import List
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, PickleType
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, PickleType, Float
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from .database import Base
 
@@ -29,3 +29,16 @@ class Project(Base):
     contents: Mapped[List[str]] = mapped_column(PickleType)
     owner_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
     created_at: Mapped[str] = mapped_column(DateTime, default=datetime.now())
+
+
+class Courses(Base):
+    __tablename__ = "courses"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    image: Mapped[str] = mapped_column(String)
+    author: Mapped[str] = mapped_column(String)
+    rating: Mapped[float] = mapped_column(Float)
+    rating_count: Mapped[int] = mapped_column(Integer)
+    tags: List[str] = mapped_column(PickleType)
+    level: Mapped[int] = mapped_column(Integer)
