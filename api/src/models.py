927d87a0fd1d262dd7d228857aef30dc6e505fb5
piotr <piotr.las@protonmail.com>
2023-09-30 21:40:57 +0200

    Update models and schemas

--- api/src/models.py
+++ api/src/models.py
@@ -1,5 +1,5 @@
 from typing import List
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from .database import Base
 
@@ -11,15 +11,19 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String)
     fullname: Mapped[str] = mapped_column(String)
     email: Mapped[str] = mapped_column(String)
+    username: Mapped[str] = mapped_column(String)
+    role: Mapped[str] = mapped_column(String)
+    projects: Mapped[List["Project"]] = relationship()
+    created_at: Mapped[str] = mapped_column(String)
 
 
 class Project(Base):
     __tablename__ = "projects"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String)
+    title: Mapped[str] = mapped_column(String)
     description: Mapped[str] = mapped_column(String)
-
+    owner_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
+    created_at: Mapped[str] = mapped_column(String)
--- api/src/schemas.py
+++ api/src/schemas.py
@@ -1,6 +1,20 @@
+from typing import List
 from pydantic import BaseModel
 
 
 class Project(BaseModel):
-    name: str
+    id: int
+    title: str
     description: str
+    owner_id: int = None
+    created_at: str
+
+
+class User(BaseModel):
+    id: int
+    fullname: str
+    email: str
+    username: str
+    projects: List[Project] = None
+    role: str
+    created_at: str
