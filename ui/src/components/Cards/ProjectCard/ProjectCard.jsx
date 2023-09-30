6ca2134e11985c278d6aa0b94124c3ade1b8cdf4
SafeHeaded <m.patryk@outlook.com>
2023-09-30 23:39:08 +0200

    Added tags gap

--- ui/src/components/Cards/ProjectCard/ProjectCard.jsx
+++ ui/src/components/Cards/ProjectCard/ProjectCard.jsx
@@ -3,10 +3,10 @@ import default_project_image from '../../../assets/images/default_project_photo.
 import Typography from "@mui/material/Typography";
 import {tagsColorsMap} from "../../../utils/tagsColorsMap.js";
 
-export function ProjectCard({title, description, image, id, tags}) {
+export function ProjectCard({title, description, photo, id, tags}) {
     return <Card sx={{width: '350px'}}>
         <CardMedia
-            sx={{ height: 280 }} image={image || default_project_image} />
+            sx={{ height: 280 }} image={photo || default_project_image} />
         <CardContent>
             <Typography gutterBottom variant="h5" component="div">
                 {title}
@@ -14,7 +14,7 @@ export function ProjectCard({title, description, image, id, tags}) {
             <Typography variant="body2" color="text.secondary">
                 {description}
             </Typography>
-            <Stack direction="row">
+            <Stack direction="row" gap="5px">
                 {(tags || []).map((tag) => <Chip style={{backgroundColor: tagsColorsMap.get(tag) || 'blue'}} size="small" key={tag} label={`#${tag}`} />)}
             </Stack>
         </CardContent>
