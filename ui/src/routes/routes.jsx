92abf0bd20948c5c3cb75c7f8849e4b5f06565af
SafeHeaded <m.patryk@outlook.com>
2023-10-01 08:59:26 +0200

    merge fixes

--- ui/src/routes/routes.jsx
+++ ui/src/routes/routes.jsx
@@ -4,10 +4,6 @@ import { Projects, projectsLoader } from "../pages/Projects/Projects.jsx";
 import { MainLayout } from "../components/Layout/MainLayout.jsx";
 import { Courses } from "../pages/Courses/Courses.jsx";
 import { CourseDetails } from "../pages/CourseDetails/CourseDetails.jsx";
-import {createBrowserRouter} from "react-router-dom";
-import {LandingPage} from "../pages/LandingPage/LandingPage.jsx";
-import {Projects, projectsLoader} from "../pages/Projects/Projects.jsx";
-import {MainLayout} from "../components/Layout/MainLayout.jsx";
 import {ProjectDetails, projectDetailsLoader} from "../pages/ProjectDetails/ProjectDetails.jsx";
 
 export const router = createBrowserRouter([
@@ -16,19 +12,13 @@ export const router = createBrowserRouter([
     element: <MainLayout />,
     children: [{index: true, element: <LandingPage/>
 }, {path: '/projects', element: <Projects />, loader: projectsLoader},
-        {path: '/projects/:id', element: <ProjectDetails />, loader: projectDetailsLoader}]
-}])
-    children: [
-      { index: true, element: <LandingPage /> },
-      { path: "/projects", element: <Projects />, loader: projectsLoader },
-      {
+        {path: '/projects/:id', element: <ProjectDetails />, loader: projectDetailsLoader}, {
         path: "/courses",
         element: <Courses />,
       },
       {
         path: "/courses/:id",
         element: <CourseDetails />,
-      },
-    ],
-  },
-]);
+      },]
+},]);
+
